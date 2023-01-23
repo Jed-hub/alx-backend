6ca65e6021125753c7457cc2ab67cb019fb5f3a6
root <jedidiabahena@gmail.com>
2023-01-23 13:35:00 +0000

    added file 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Simple helper module """
+import csv
+import math
+
+from typing import Tuple, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            """
+            List of pagination
+            """
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
+
+            range: Tuple = index_range(page, page_size)
+            pagination: List = self.dataset()
+
+            return (pagination[range[0]:range[1]])
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size containing a start index and
+    an end index corresponding to the range of indexes
+    """
+    end: int = page * page_size
+    start: int = end - page_size
+
+    return (start, end)
