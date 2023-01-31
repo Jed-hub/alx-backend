d0e7fbd216fbdc179098856fd2c60b3921eb9887
root <jedidiabahena@gmail.com>
2023-01-31 16:41:44 +0000

    added app 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """
+    Configuration of available languages in the app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with our supported language
+    """
+    locale = request.args.get('locale', None)
+
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    Hello world
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
