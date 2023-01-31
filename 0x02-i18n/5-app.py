73c0b57a6981a914d5942c94f0fa7697cee0aff5
root <jedidiabahena@gmail.com>
2023-01-31 17:03:52 +0000

    added app 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, g, request, render_template
+from flask_babel import Babel, _
+from typing import Union
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """
+    Configuration of available languages in the app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.before_request
+def before_request(login_as: int = None):
+    """
+    Before other functions
+    """
+    user: dict = get_user()
+    print(user)
+    g.user = user
+
+
+def get_user() -> Union[dict, None]:
+    """
+    Return user from the dict
+    """
+    login_user = request.args.get('login_as', None)
+
+    if login_user is None:
+        return None
+
+    user: dict = {}
+    user[login_user] = users.get(int(login_user))
+
+    return user[login_user]
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with our supported language
+    """
+    locale = request.args.get('locale', None)
+
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    Hello world
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
