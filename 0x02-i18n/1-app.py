14346a758012c64381b4506b17149a26f9073f98
root <jedidiabahena@gmail.com>
2023-01-31 15:31:06 +0000

    added apps

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Configuration of available languages in the app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """
+    Hello world
+    """
+    return render_template('1-index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
